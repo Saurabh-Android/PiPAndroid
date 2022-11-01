e4bd8849e30d369d32d1dd0cb6c6c509f918afd2
Saurabh-Android <saurabhgupta933@gmail.com>
2022-11-01 18:23:11 +0530

    Intial commit into master

---
+++ app/src/main/java/com/android/mypipmode/MainActivity.java
@@ -0,0 +1,23 @@
+package com.android.mypipmode;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        LocalBroadcastManager.getInstance(this)
+                .sendBroadcast(Intent(PipActivity.ACTION_FINISH_PIP_MODE))
+
+        findViewById<Button>(R.id.pip_activity_button).setOnClickListener {
+            startActivity(Intent(this, MyPipActivity::class.java))
+        }
+    }
+}
---
+++ app/src/main/java/com/android/mypipmode/MyPipActivity.java
@@ -0,0 +1,14 @@
+package com.android.mypipmode;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyPipActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_pip);
+    }
+}
